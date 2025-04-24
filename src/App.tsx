a1aecaaa89985e0da4adb28740286e5b394a19a2
Willian Oliveira <willian.oliveira737@gmail.com>
2025-04-23 23:50:07 -0300

    Add react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
 
 export default function App() {
+  const [name, setName] = useState("");
+
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    setName("");
+  }
+
   return (
-    <div>
+    <div className="container">
       <h1>Evento</h1>
 
-      <form>
-        <input type="text" placeholder="Nome do evento" />
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Nome do evento"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <span className="error">Nome é obrigatório</span>
 
         <input type="date" placeholder="Nome do evento" lang="pt-BR" />
@@ -27,5 +41,5 @@ export default function App() {
         <button type="submit">Salvar</button>
       </form>
     </div>
-  )
+  );
 }
